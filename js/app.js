f1502710768dc03083103969a3ab39b6bc6c9ece
rc4b <rcaborelli@gmail.com>
2024-12-12 15:35:33 -0300

    Inicia projeto Carrinho de Compras

---
+++ js/app.js
@@ -0,0 +1,33 @@
+// Carregando o site com todos os valores zerados
+let valorTotal = 0;
+limpar();
+
+function adicionar() {
+    //recuperarando valores: nome do produto, quantidade e valor 
+    let produto = document.getElementById('produto').value;
+    let nomeProduto = produto.split('-')[0];
+    let valorProduto = parseFloat(produto.split('R$')[1]);
+    let quantidade = document.getElementById('quantidade').value;
+
+    // calculando o subtotal
+    let preco = quantidade * valorProduto;
+
+    //atualizando os produtos no carrinho
+    let produtosCarrinho = document.getElementById('lista-produtos');
+    produtosCarrinho.innerHTML = produtosCarrinho.innerHTML + (`<section class="carrinho__produtos__produto">
+          <span class="texto-azul">${quantidade}</span> ${nomeProduto} <span class="texto-azul">R$${preco}</span>
+        </section>`);
+
+    // Calculando o valor total
+    valorTotal = valorTotal + preco;
+    let total = document.getElementById('valor-total');
+    total.textContent =`R$${valorTotal}`;
+    document.getElementById('quantidade').value = '';
+    
+};
+// Limpa o carrinho de compras
+function limpar () {
+  valorTotal = 0
+  document.getElementById('lista-produtos').innerHTML = '';
+  document.getElementById('valor-total').textContent = 'R$ 0';
+};
